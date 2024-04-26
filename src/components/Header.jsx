d70a36fbeeecd2f1e25ef3e73473b40594e50919
venya2001 <ve.po2014@yandex.ru>
2024-04-26 13:07:20 +0300

    Headers was added. VITE + React init.

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="logo">VenyaPy</div>
+            <div className="sections">
+                <ul>
+                    <li className="section">Обо мне</li>
+                    <li className="section">Мои проекты</li>
+                    <li className="section">Связаться</li>
+                </ul>
+            </div>
+            <div className="spacer"></div>
+        </header>
+    )
+}
